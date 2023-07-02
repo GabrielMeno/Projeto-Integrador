8246ca4161a556bd044a02f7726a4bd3f4f84aed
Gabriel Henrique Menoncin <gabriel.menoncin@estudante.uffs.edu.br>
2023-07-02 14:28:23 -0300

    pagina de login completa/incio pagina de register

--- api/src/routes/users.routes.ts
+++ api/src/routes/users.routes.ts
@@ -1,7 +1,7 @@
-import { Router } from 'express';
-import { UsersController } from '../controllers/UsersControllers';
-import { upload } from '../config/multer';
-import { AuthMiddleware } from '../middlewares/AuthMiddleware';
+import { Router } from "express";
+import { UsersController } from "../controllers/UsersControllers";
+import { upload } from "../config/multer";
+import { AuthMiddleware } from "../middlewares/AuthMiddleware";
 class UsersRoutes {
   private router: Router;
   private usersController: UsersController;
@@ -13,23 +13,23 @@ class UsersRoutes {
   }
   getRoutes(): Router {
     this.router.post(
-      '/',
-      this.usersController.store.bind(this.usersController),
+      "/",
+      this.usersController.store.bind(this.usersController)
     );
     this.router.put(
-      '/',
-      upload.single('avatar_url'),
+      "/",
+      upload.single("avatar_url"),
       this.authMiddleware.auth.bind(this.authMiddleware),
-      this.usersController.update.bind(this.usersController),
+      this.usersController.update.bind(this.usersController)
     );
 
     this.router.post(
-      '/auth',
-      this.usersController.auth.bind(this.usersController),
+      "/auth",
+      this.usersController.auth.bind(this.usersController)
     );
     this.router.post(
-      '/refresh',
-      this.usersController.refresh.bind(this.usersController),
+      "/refresh",
+      this.usersController.refresh.bind(this.usersController)
     );
 
     return this.router;
--- front/src/page/Login/index.tsx
+++ front/src/page/Login/index.tsx
@@ -1,8 +1,29 @@
 import style from "./Login.module.css";
 import logo from "../../assets/logo.png";
 import { Input } from "../../components/Input/Index";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface IFormValues {
+  email: string;
+  password: string;
+}
 
 export function Login() {
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Digite um email válido")
+      .required("Campo de email obrigadotório"),
+    password: yup.string().required("Campo de senha obrigatório"),
+  });
+  const { register, handleSubmit, formState: {errors} } = useForm<IFormValues>({
+    resolver: yupResolver(schema),
+  });
+  const submit = handleSubmit((data) => {
+    console.log(data);
+  });
   return (
     <div className={style.background}>
       <div className={`container ${style.container}`}>
@@ -12,9 +33,18 @@ export function Login() {
           </div>
           <div className={style.card}>
             <h2>Olá, Seja bem vindo</h2>
-            <form>
-              <Input/>
-              <input type="text" />
+            <form onSubmit={submit}>
+              <Input
+                type="text"
+                placeholder="Email"
+                {...register("email", { required: true })}
+                error={errors.email && errors.email.message }
+              />
+              <Input
+                placeholder="Senha"
+                type="password"
+                {...register("password", { required: true })}
+              />
               <button>Entrar</button>
             </form>
           </div>
