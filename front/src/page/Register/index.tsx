62bff5f4837f6a0ae055b7b341ed983de3236233
Gabriel Henrique Menoncin <gabriel.menoncin@estudante.uffs.edu.br>
2023-07-02 16:05:43 -0300

    iniciando tela de cadastro

---
+++ front/src/page/Register/index.tsx
@@ -0,0 +1,11 @@
+import style from './Register.module.css';
+
+export function Register() {
+  return (
+    <div className={style.background}>
+      <div className="container">
+        <p className={style.navigate}>home {'>'} Área de Cadastro</p>
+      </div>
+    </div>
+  );
+}
--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "../page/Login";
+import { Register } from "../page/Register";
 
 export const RouteApp = () => {
   return (
-      <Routes>
-        <Route
-          path="/"
-          element={<Login/>}
-        />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 };
