0de18c704af65d44c7f54209cf9e90822c96e739
Gabriel Henrique Menoncin <gabriel.menoncin@estudante.uffs.edu.br>
2023-07-09 20:54:49 -0300

    tentando corrigir a rota para dashbor

--- front/src/server/index.ts
+++ front/src/server/index.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { Axios, AxiosError } from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -13,7 +13,34 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+api.interceptors.response.use(
+  (response) => response, 
+  async (error: AxiosError | unknown) => {
 
+    const originalRequest = (error as AxiosError).config;
 
+    if(error instanceof AxiosError && error.response?.status === 401) {
+      if(
+        error.response?.data && 
+        error.response?.data.code === 'token.expired'
+      ) {
+        const refresh = localStorage.getItem(
+          'refresh_token: projeto-integrador',
+          );
+        const response = await api.post('/refresh', {
+          refresh_token: refresh
+        });
 
+        const { token, refresh_token } = response.data;
+        localStorage.setItem('token: projeto-integrador', token);
+        localStorage.setItem('refresh_token: projeto-integrador', refresh_token);
+
+        if(originalRequest?.headers)
+          originalRequest.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+  }
+);
+        
 export { api };
+ 
