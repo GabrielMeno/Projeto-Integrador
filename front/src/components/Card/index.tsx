8384b5c7ae16d840cf3500d7ded6d8f6d566717a
Gabriel Henrique Menoncin <gabriel.menoncin@estudante.uffs.edu.br>
2023-07-16 23:54:34 -0300

    .. dashboard

--- front/src/components/Card/index.tsx
+++ front/src/components/Card/index.tsx
@@ -1,19 +1,39 @@
 import style from "./Card.module.css";
 import { RiDeleteBinLine } from "react-icons/ri";
-
 import { AiOutlineEdit } from "react-icons/ai";
-export const Card = () => {
+import { getHours, isAfter } from "date-fns";
+
+interface ISchedule {
+  name: string;
+  date: Date;
+  phone: string;
+  id: string;
+}
+
+export const Card = ({ name, date, id, phone }: ISchedule) => {
+  const isAfterDate = isAfter(new Date(date), new Date());
+
+  let phoneFormated = phone.replace(/\D/g, "");
+  phoneFormated = phoneFormated.replace(
+    /(\d{2})(\d{4})(\d{4})/,
+    "($1)  $2-$3"
+  );
+
+  console.log("🚀 ~ file: index.tsx:16 ~ Card ~ isAfterDate:", isAfterDate);
   return (
-    <div  className={style.background}>
-      
+    <div className={style.background}>
       <div>
-        <span>10h</span>
-        <p>Gabriel Menoncin</p>
+        <span className={`${!isAfterDate && style.disabled}`}>
+          {getHours(new Date(date))}h
+        </span>
+        <p>
+          {name} - {phoneFormated}
+        </p>
       </div>
       <div className={style.icons}>
-        <AiOutlineEdit color="#5F68B1" size={17}/>
-        <RiDeleteBinLine  color= "#EB2E2E" size={17}/>  
+        <AiOutlineEdit color="#5F68B1" size={17} />
+        <RiDeleteBinLine color="#EB2E2E" size={17} />
       </div>
     </div>
-  )
-}
+  );
+};
--- front/src/page/Dashboard/index.tsx
+++ front/src/page/Dashboard/index.tsx
@@ -3,16 +3,25 @@ import { Card } from "../../components/Card";
 import { Header } from "../../components/Header";
 import { useAuth } from "../../hooks/auth";
 import style from "./Dashboard.module.css";
-import  {DayPicker} from 'react-day-picker'
-import 'react-day-picker/dist/style.css';
-import {ptBR} from 'date-fns/locale';
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { ptBR } from "date-fns/locale";
 import { format, isToday } from "date-fns";
 import { api } from "../../server";
 
+interface IShedule {
+  name: string;
+  date: Date;
+  phone: string;
+  id: string;
+}
 
 export function Dashboard() {
-  const [date, setDate] = useState(new Date())
-  const { user}= useAuth();
+  const [date, setDate] = useState(new Date());
+  const { user } = useAuth();
+
+  const [schedules, setSchedules] = useState<Array<IShedule>>([]);
+
   const isWeekend = (date: Date) => {
     const day = date.getDay();
     return day === 0 || day === 7;
@@ -24,55 +33,70 @@ export function Dashboard() {
   };
 
   const handleDataChange = (date: Date) => {
-    setDate(date)
-  }
+    setDate(date);
+  };
 
   useEffect(() => {
-    api.post('/schedules', {})
-  })
+    api
+      .get("/schedules", {
+        params: {
+          date,
+        },
+      })
+      .then((response) => {
+        console.log(
+          "🚀 ~ file: index.tsx:36 ~ useEffect ~ response:",
+          response
+        );
+        setSchedules(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, [date]);
 
   return (
     <div className="container">
-      <Header/>
+      <Header />
       <div className={style.dataTitle}>
         <h2>Bem Vindo(a) {user.name}</h2>
-        <p> Está é a sua lista de horários para {isToday(date) && <span>de hoje, </span>} dia {format(date, 'dd/MM/yyy')}</p>
+        <p>
+          {" "}
+          Está é a sua lista de horários para{" "}
+          {isToday(date) && <span>de hoje, </span>} dia{" "}
+          {format(date, "dd/MM/yyy")}
+        </p>
       </div>
       <h2 className={style.nextSchedules}>Próximos Horários </h2>
       <div className={style.schedule}>
         <div className={style.cardWrapper}>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+          {schedules.map((schedule, index) => {
+            return (
+              <Card
+                key={index}
+                date={schedule.date}
+                name={schedule.name}
+                id={schedule.id}
+                phone={schedule.phone}
+              />
+            );
+          })}
         </div>
         <div className={style.picker}>
-          <DayPicker 
-          className={style.calendar}  
-          classNames={{
-            day: style.day,
-          }} 
-          selected={date} 
-          modifiers= {{available: isWeeDay}} 
-          mode="single"
-          modifiersClassNames={{
-            selected: style.selected,
-          }}
-          fromMonth={new Date()}
-          locale={ptBR}
-          disabled= {isWeekend}
-          onDayClick={handleDataChange}
+          <DayPicker
+            className={style.calendar}
+            classNames={{
+              day: style.day,
+            }}
+            selected={date}
+            modifiers={{ available: isWeeDay }}
+            mode="single"
+            modifiersClassNames={{
+              selected: style.selected,
+            }}
+            fromMonth={new Date()}
+            locale={ptBR}
+            disabled={isWeekend}
+            onDayClick={handleDataChange}
           />
-          
         </div>
       </div>
     </div>
